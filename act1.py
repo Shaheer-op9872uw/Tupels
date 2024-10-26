9d4cf084e31d8d7f92745235e59c0033337863bd
Shaheer-op9872uw <shaheerminhas123@gmail.com>
2024-10-26 20:38:56 +0500

    I Learned about operations on tuples

---
+++ act1.py
@@ -0,0 +1,13 @@
+tuplex = ("teple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = (2, 3, 2, 4, 2, 9)
+print(tuplex)
+tuplex = tuplex + (9,)
+print (tuplex)
+tuple1 = (40, 43, 31, 43, 52)
+print(tuple1.count(40))
+tuplex = (2, 4, 2, 4, 5 , 3, 2 , 8)
+_slice = tuplex[2:4]
+print(_slice)
+
